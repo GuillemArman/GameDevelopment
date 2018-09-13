37a299f0b7c9a22b54850391275701f1e3be8e9b
GuillemArman <guillemarman8@gmail.com>
2018-09-13 20:29:32 +0200

    vec3 header created
    methods and operators

---
+++ Vector3Class/Vector3Class/Main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+#include "vec3_class.h"
+
+using namespace std;
+
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Vector3Class/Vector3Class/vec3_class.h
@@ -0,0 +1,151 @@
+#ifndef VEC3_CLASS
+#define VEC3_CLASS
+
+template <class T>
+
+class vec3
+{
+public:
+	V x;
+	V y;
+	V z;
+
+public:
+	// Constructors 
+	vec3<T>(V _x, V _y, V _z)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+	vec3<T>:x(0)y(0)z(0) {}
+
+	//Destructor
+	//vec3<T>::~vec3<T>
+
+
+	// Operators
+	vec3<T> operator+(vec3<T> vector) const;
+	vec3<T> operator-(vec3<T> vector) const;
+	void operator+=(vec3<T> vector);
+	void operator-=(vec3<T> vector);
+	void operator=(vec3<T> vector) ;
+	void operator==(vec3<T> vector) const;
+
+
+	// Methods
+	void normalize();
+	void zero();
+	bool is_zero();
+	double distance_to(vec3<T> vector);
+	double get_module();
+	vec3<T> get_vector_from(vec3<T> vector);
+
+
+
+};
+
+
+// OPERATOR +
+template<class T>
+vec3<T> vec3<T>::operator+(vec3<T> vector) const  
+{
+	vec3<T> ret(x + vector.x, y + vector.y, z + vector.z);
+	return ret;
+}
+
+
+// OPERATOR - 
+template<class T>
+vec3<T> vec3<T>::operator-(vec3<T> vector) const
+{
+	vec3<T> ret(x - vector.x, y - vector.y, z - vector.z);
+	return ret;
+}
+
+// OPERATOR +=
+template<class T>
+void vec3<T>::operator+=(vec3<T> vector)
+{
+	x += vector.x;
+	y += vector.y;
+	z += vector.z;
+}
+
+// OPERATOR -=
+template<class T>
+void vec3<T>::operator-=(vec3<T> vector)
+{
+	x -= vector.x;
+	y -= vector.y;
+	z -= vector.z;
+}
+
+// OPERATOR =
+template<class T>
+void vec3<T>::operator=(vec3<T> vector) 
+{
+	x = vector.x;
+	y = vector.y;
+	z = vector.z;
+}
+
+// OPERATOR ==
+template<class T>
+bool vec3<T>::operator==(vec3<T> vector) const
+{
+	return x == vector.x && y == vector.y && z == vector.z;
+
+}
+
+//METHODS
+
+template<class T>
+void vec3<T>::normalize()
+{
+	vec3<T> module = this;
+
+	x = x / module.get_module();
+	y = y / module.get_module();
+	z = z / module.get_module();
+}
+
+template<class T>
+void vec3<T>::zero()
+{
+	x = 0;
+	y = 0;
+	z = 0;
+}
+
+template<class T>
+bool vec3<T>::is_zero()
+{
+	return x == 0 && y == 0 & z == 0;
+}
+
+template<class T>
+double vec3<T>::distance_to(vec3 vector)
+{
+	int dx, dy, dz;
+	dx = x - vector.x;
+	dy = y - vector.y;
+	dz = z - vector.z;
+	return sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
+}
+
+template<class T>
+double vec3<T>::get_module()
+{
+	return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+}
+
+template<class T>
+vec3<T> vec<T>::get_vector_from(vec3 vector)
+{
+	vec3 newvector = vec3(x - vector.x, y - vector.y, z - vector.z);
+	return newvector;
+}
+
+#endif 
+
