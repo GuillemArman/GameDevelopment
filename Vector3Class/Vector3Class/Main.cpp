37d47c14b02e081172f38ea6a9f65f756aeb0d05
GuillemArman <guillemarman8@gmail.com>
2018-09-14 20:57:24 +0200

    Testing class in main

--- Vector3Class/Vector3Class/Main.cpp
+++ Vector3Class/Vector3Class/Main.cpp
@@ -7,7 +7,39 @@ using namespace std;
 
 int main()
 {
+	vec3<int> vector_1 = vec3<int>(6, 4, 7);
+	vec3<int> vector_2 = vec3<int>(2, 1, 7);
+	vec3<int> vector_3 = vec3<int>(6, 4, 7);
+
+	printf("vector 1 = %i, %i, %i\n", vector_1.x, vector_1.y, vector_1.z);
+	printf("vector 2 = %i, %i, %i\n", vector_2.x, vector_2.y, vector_2.z);
+	printf("Normalize vector 1: %f\n", vector_1.get_module());
+
+	vector_1.zero();
+
+	printf("vector 1 = %i, %i, %i\n", vector_1.x, vector_1.y, vector_1.z);
+
+	if (vector_1.is_zero())
+	{
+		printf("vector 1 = 0? : Yes\n");
+	}
+	else
+	{
+		printf("vector 1 = 0? : No\n");
+	}
+
+	if (vector_2.is_zero())
+	{
+		printf("vector 2 = 0? : Yes\n");
+	}
+	else
+	{
+		printf("vector 2 = 0? : No\n");
+	}
+
+	printf("Distance between vectors 1 and 2 = %f\n", vector_1.distance_to(vector_2));
 
 
+	system("pause");
 	return 0;
 }
